3fa344e4fc06a4632c5c2c0d3d25ad2e13cc48e0
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-05-24 12:52:07 +0530

    Time: 2 ms (71.03%) | Memory: 4.3 MB (82.44%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.go
+++ 5-longest-palindromic-substring/longest-palindromic-substring.go
@@ -1,32 +1,27 @@
 func longestPalindrome(s string) string {
     n := len(s)
-    dp := make([][]bool, n)
-    for i := range dp{
-        dp[i] = make([]bool, n)
+
+    expand := func (l, r int) string {
+        for l>=0 && r<n && s[l] == s[r] {
+            l--
+            r++
+        }
+        return s[l+1: r]
     }
 
-    ans := []int{0, 0}
+    ans := ""
 
     for i:=0; i<n; i++ {
-        dp[i][i] = true
-    }
-
-    for i:=0; i<n-1; i++ {
-        if s[i] == s[i+1]{
-            dp[i][i+1] = true
-            ans = []int{i, i+1}
+        odd := expand(i, i)
+        if len(odd) > len(ans){
+            ans = odd
         }
-    }
-    
-    for d:=2 ; d<n; d++ {
-        for i:=0; i<n-d; i++ {
-            j := i + d
-            if s[i] == s[j] && dp[i+1][j-1] {
-                dp[i][j] = true
-                ans = []int{i, j}
-            }
+
+        even := expand(i, i+1)
+        if len(even) > len(ans){
+            ans = even
         }
     }
 
-    return s[ans[0]: ans[1]+1]
+    return ans
 }
