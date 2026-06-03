1203356139cd0b6c8ac7ce0d5669483c282f7945
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-06-03 14:50:40 +0530

    Time: 12 ms (53.53%) | Memory: 8.4 MB (66.42%) - LeetSync

--- 322-coin-change/coin-change.go
+++ 322-coin-change/coin-change.go
@@ -1,31 +1,20 @@
 func coinChange(coins []int, amount int) int {
-    n := len(coins)
-    // sort.Ints(coins)
-
-    dp := make([][]int, n+1)
-    for i:=0; i<=n; i++{
-        dp[i] = make([]int, amount+1)
+    // dp[a] = Min no. of coins required to pick, to be able to form an amount 'a'
+    dp := make([]int, amount+1)
+    for i := range dp {
+        dp[i] = amount+1
     }
-
+    dp[0] = 0
     for a:=0; a<=amount; a++ {
-        dp[0][a] = amount+1
-    }
-    for i:=0; i<=n; i++ {
-        dp[i][0] = 0
-    }
-
-    for i:=1; i<=n; i++ {
-        for a:=1; a<=amount; a++ {
-            dp[i][a] = dp[i-1][a]
-            if a >= coins[i-1] {
-                dp[i][a] = min(dp[i][a], dp[i][a-coins[i-1]]+1)
+        for _, c := range coins {
+            if a >= c {
+                dp[a] = min(dp[a], dp[a-c]+1)
             }
         }
     }
 
-    if dp[n][amount] > amount {
+    if dp[amount] > amount {
         return -1
-    }else{
-        return dp[n][amount]
     }
+    return dp[amount]
 }
