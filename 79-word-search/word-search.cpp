0ae07c61f9e6bfc1cab57169bfbb7d242f2c2d21
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-08-14 16:46:57 +0530

    Time: 275 ms (48.62%) | Memory: 10.5 MB (64.68%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,42 +1,41 @@
 class Solution {
-
 private:
-    int m, n, l;
-    string word;
+    int m, n;
+    string s;
     vector<vector<char>> board;
+    vector<pair<int, int>> directions = {{0, -1}, {0, +1}, {-1, 0}, {1, 0}};
 
-    bool backtrack(int i, int j, int curr){
-        // curr == current index of the word
-        if(curr >= l) return true;
-        if(i<0 || i>=m || j<0 || j>=n) return false;
+    bool dfs(int i, int j, int ind){
+        // if(i<0 || j<0 || i>=m || j>=n) return false;
+        if(ind == s.length()-1 && board[i][j] == s[ind]) return true;
+        if(board[i][j] != s[ind]) return false;
+        
+        char temp = board[i][j];
+        board[i][j] = '-';
 
-        if(word[curr] == board[i][j]){
-            board[i][j] = '#';
-            if(backtrack(i-1, j, curr+1) || backtrack(i, j+1, curr+1) || backtrack(i+1, j, curr+1) || backtrack(i,j-1, curr+1)) return true;
-
-            board[i][j] = word[curr];
-            return false;
+        for(auto [di, dj]: directions){
+            if(i+di<0 || j+dj<0 || i+di>=m || j+dj>=n) continue;
+            if(board[i+di][j+dj] != '-' && dfs(i+di, j+dj, ind+1)) return true;
         }
+
+        board[i][j] = temp;
 
         return false;
     }
-    
 
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        this->m = board.size();
-        this->n = board[0].size();
-        this->l = word.size();
-        this->word = word;
+        s = word;
         this->board = board;
+        m = board.size();
+        n = board[0].size();
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
-                }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(dfs(i, j, 0)) return true;
             }
         }
+
         return false;
     }
 };
