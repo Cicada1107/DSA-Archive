f1ec7ffdc7c99713ccd721c93b1a35846a52bb05
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-09-03 20:22:23 +0530

    Time: 6 ms (38.80%) | Memory: 18.9 MB (36.39%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        // directed graph b to a
-        vector<vector<int>> graph(n);
-        for(auto &edge: prerequisites){
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //topological sort
+        vector<int> inDegree(numCourses, 0);
+        vector<vector<int>> graph(numCourses);
+        
+        for(auto edge: prerequisites){
             int u = edge[0], v = edge[1];
             graph[v].push_back(u);
-        }
 
-        vector<int> indegree(n, 0);
-        for(int node=0; node<n; node++){
-            for(auto &neigh: graph[node]){
-                indegree[neigh]++;
-            }
+            inDegree[u]++;
         }
 
         queue<int> q;
-        for(int node=0; node<n; node++){
-            if(indegree[node]==0) q.push(node);
+        vector<int> ans;
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i] == 0) q.push(i);
         }
 
-        vector<int> ans;
         while(!q.empty()){
-            int node = q.front();
+            int curr = q.front();
             q.pop();
-            ans.push_back(node);
 
-            for(auto &neigh: graph[node]){
-                indegree[neigh]--;
-                if(indegree[neigh] == 0) q.push(neigh);
+            ans.push_back(curr);
+
+            for(auto neigh: graph[curr]){
+                inDegree[neigh]--;
+                if(inDegree[neigh] == 0) q.push(neigh);
             }
         }
 
-        if(ans.size() != n) return {};
+        if(ans.size() != numCourses) return {};
         return ans;
     }
 };
