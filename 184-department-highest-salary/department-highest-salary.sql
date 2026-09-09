0ae62af65599a5e8639af3ac2b4f8da44c778683
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-09-09 17:47:38 +0530

    Time: 727 ms (69.95%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
 SELECT d.name as DEPARTMENT, e.name as Employee, e.salary as Salary
-FROM employee e INNER JOIN department d ON e.departmentId = d.id
+FROM employee e LEFT JOIN department d ON e.departmentId = d.id
 WHERE (e.departmentId, salary) IN 
 (
     SELECT departmentId, MAX(salary)
