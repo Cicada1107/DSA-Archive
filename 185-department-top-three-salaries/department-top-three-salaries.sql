22c0c00a0e3044081f21f3fe1bef554a1be6868a
Arijit Dubey <146376696+Cicada1107@users.noreply.github.com>
2026-09-11 18:17:21 +0530

    Time: 1039 ms (68.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select d.name as Department, e.name as Employee, e.salary as Salary
+from(
+    select name, salary, departmentId, dense_rank() over(partition by departmentId order by salary desc) as rnk
+    from employee
+) e inner join Department d on e.departmentId = d.id
+where e.rnk <= 3;
